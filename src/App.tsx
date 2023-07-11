44f09182855c7dfe936c1c88d852ef90761ee717
Pivi <pivi@pivis-macbook-air.home>
2023-07-11 11:28:37 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@massalabs/react-ui-kit/src";
+
+function App() {
+  return (
+    <div>
+      <Button>this is a button</Button>
+    </div>
+  );
+}
+
+export default App;
