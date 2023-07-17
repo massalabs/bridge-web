4852feb9de87c0a4265a0ffc0da6b491ea4a6e07
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-07-17 10:23:44 +0200

    fix layout (#22)
    Co-authored-by: Pivi <pivi@pivis-macbook-air.home>

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,16 @@
+import { Button, Dropdown, ThemeMode } from '@massalabs/react-ui-kit';
+import logo from '../assets/logo.svg';
 export function Layout({ ...props }) {
   const { children } = props;
+
+  const options = [
+    {
+      item: 'BuildNet',
+    },
+    {
+      item: 'TestNet',
+    },
+  ];
 
   return (
     <div
@@ -7,7 +18,18 @@ export function Layout({ ...props }) {
       bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] 
       from-[#3271A5] to-deep-blue to-60%`}
     >
-      {children}
+      {/* Header Element */}
+      <div className="flex flex-row items-center justify-between p-11">
+        <img src={logo} alt="" />
+        <div className="flex flex-row items-center gap-4">
+          <Dropdown options={options} />
+          <Button>Connect Wallet</Button>
+          <ThemeMode />
+        </div>
+      </div>
+      <div className={`min-h-screen flex flex-col justify-center items-center`}>
+        {children}
+      </div>
     </div>
   );
 }
