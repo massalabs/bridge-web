0eb871af9e5f9c9cb50e71be8e7145d1388b06f2
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-07-17 10:32:41 +0200

    Add Header In Layout (#23)
    * fix layout (#22)
    
    Co-authored-by: Pivi <pivi@pivis-macbook-air.home>
    
    * Add Fixed Height
    
    ---------
    
    Co-authored-by: Pivi <pivi@pivis-macbook-air.home>

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,36 @@
+import { Button, Dropdown, ThemeMode } from '@massalabs/react-ui-kit';
+import logo from '../assets/logo.svg';
 export function Layout({ ...props }) {
   const { children } = props;
 
+  const options = [
+    {
+      item: 'BuildNet',
+    },
+    {
+      item: 'TestNet',
+    },
+  ];
+
   return (
     <div
-      className={`min-h-screen flex flex-col justify-center items-center
-      bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] 
-      from-[#3271A5] to-deep-blue to-60%`}
+      className="bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] 
+      from-[#3271A5] to-deep-blue to-60%
+      fixed w-full
+      "
     >
-      {children}
+      {/* Header Element */}
+      <div className="flex flex-row items-center justify-between p-11 h-fit ">
+        <img src={logo} alt="" />
+        <div className="flex flex-row items-center gap-4">
+          <Dropdown options={options} />
+          <Button>Connect Wallet</Button>
+          <ThemeMode />
+        </div>
+      </div>
+      <div className={`min-h-screen flex flex-col justify-center items-center`}>
+        {children}
+      </div>
     </div>
   );
 }
