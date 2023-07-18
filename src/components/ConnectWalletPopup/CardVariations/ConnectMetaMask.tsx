59d7a0f0d1f1d7cd4ccfa34b3beee8d2d793d71c
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-07-18 10:24:05 +0200

    fix main (#48)
    Co-authored-by: Pivi <pivi@Pivis-MacBook-Air.local>

--- src/components/ConnectWalletPopup/CardVariations/ConnectMetaMask.tsx
+++ src/components/ConnectWalletPopup/CardVariations/ConnectMetaMask.tsx
@@ -1,7 +1,7 @@
 export function ConnectMetamask({ ...props }) {
   // We'll need to add the mechnism that checks to see if metamask is connected
 
-  const { src } = props;
+  const { src, isMetamaskConnected } = props;
   return (
     <div className="flex w-full gap-4">
       <div className="flex flex-col justify-between items-center w-fit">
@@ -12,7 +12,7 @@ export function ConnectMetamask({ ...props }) {
       <div className="mas-menu-default">
         If you want to get metamask <br /> <br /> <br />
         <a href="www.mylink.com" target="_blank">
-          <u>Click Here</u>
+          <u onClick={() => isMetamaskConnected(true)}>Click Here</u>
         </a>
       </div>
     </div>
