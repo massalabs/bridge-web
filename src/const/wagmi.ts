abbb6a20d8d3b4fb70cb78ec0f7df00845f6f9a7
Pierre Seznec <petterseznec@gmail.com>
2023-08-01 11:55:20 +0200

    do not expose private rpc api key

--- src/const/wagmi.ts
+++ src/const/wagmi.ts
@@ -4,12 +4,17 @@ import { publicProvider } from '@wagmi/core/providers/public';
 import { configureChains, createConfig, sepolia } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 
-const ALCHEMY_API_KEY = import.meta.env['VITE_ALCHEMY_API_KEY'] || '';
+const ALCHEMY_API_KEY = import.meta.env['VITE_ALCHEMY_API_KEY'];
+
+const privateProvider = alchemyProvider({ apiKey: ALCHEMY_API_KEY })
+
 export const { chains, publicClient } = configureChains(
   [sepolia],
-  [alchemyProvider({ apiKey: ALCHEMY_API_KEY }), publicProvider()],
+  [ publicProvider()],
   { batch: { multicall: true } },
 );
+
+(chains as any).rpcUrls = privateProvider(sepolia)?.rpcUrls
 
 export const connectors = connectorsForWallets([
   {
