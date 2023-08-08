67cc07f3255664abf2275ccfe8864707305b560f
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2023-08-08 18:12:00 +0200

    bridge-web: implement all connection cases (#192)
    * bridge-web: refactor index page (#187)
    
    * bridge-web: update link to create wallet

--- src/components/ConnectWalletPopup/CardVariations/RessourceSidePanel.tsx
+++ src/components/ConnectWalletPopup/CardVariations/RessourceSidePanel.tsx
@@ -1,39 +1,93 @@
 import { Tag } from '@massalabs/react-ui-kit';
+import { useAccount } from 'wagmi';
 
 import Intl from '@/i18n/i18n';
+import { useAccountStore } from '@/store/store';
 import { linkToCreateWallet } from '@/utils/const';
 
+function SepoliaInstructions() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-5">
+      <Tag type="info" content="Add Sepolia testnet" />
+      <p className="text-center mas-menu-default w-60">
+        {Intl.t('connect-wallet.ressource-sidepanel.add-sepolia')}
+      </p>
+      <a className="mas-menu-underline" href="www.mylink.com" target="_blank">
+        {Intl.t('general.instructions')}
+      </a>
+    </div>
+  );
+}
+
+function MassaStationInstructions() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-5">
+      <Tag type="success" content="Create a Massa Wallet" />
+      <p className="text-center mas-menu-default w-60">
+        {Intl.t('connect-wallet.ressource-sidepanel.download-massa-station')}
+      </p>
+      <a className="mas-menu-underline" href="www.mylink.com" target="_blank">
+        {Intl.t('general.download')}
+      </a>
+    </div>
+  );
+}
+
 export function RessourceSidePanel() {
+  const { isConnected: isEvmWalletConnected } = useAccount();
+
+  const [accounts, isStationInstalled] = useAccountStore((state) => [
+    state.accounts,
+    state.isStationInstalled,
+  ]);
+
+  const isOnlyMetamaskConnected =
+    isEvmWalletConnected && !isStationInstalled && accounts.length === 0;
+  const isOnlyMassaConnected =
+    isStationInstalled && !isEvmWalletConnected && accounts.length > 0;
+
   return (
     <div
-      className="
-        bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))]
-        from-bright-blue to-deep-blue to-60%
-        min-h-full w-[270px] p-6 rounded-2xl
-        flex flex-col justify-evenly items-center gap-4
-        "
+      className={`
+        h-full p-6 rounded-2xl
+        bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-bright-blue to-deep-blue to-60%
+        flex flex-col justify-center items-center gap-11`}
     >
-      <div className="flex flex-col justify-center items-center gap-4 mas-menu-default">
-        <Tag type="info" content="Add Sepolia testnet" />
-        <div className="text-center">
-          {Intl.t('connect-wallet.ressource-sidepanel.add-sepolia')}
-        </div>
-        <br />
-        <a href="www.mylink.com" target="_blank">
-          <u>{Intl.t('general.click-here')}</u>
-        </a>
-      </div>
-      <div className="w-full bg-slate-500 bg-opacity-5 border border-1"></div>
-      <div className="flex flex-col justify-center items-center gap-4 mas-menu-default">
-        <Tag type="success" content="Get Massa Wallet" />
-        <div className="text-center">
-          {Intl.t('connect-wallet.ressource-sidepanel.add-massa')}
-        </div>
-        <br />
-        <a href={linkToCreateWallet} target="_blank">
-          <u> {Intl.t('general.click-here')}</u>
-        </a>
-      </div>
+      {isOnlyMetamaskConnected ? (
+        <MassaStationInstructions />
+      ) : isOnlyMassaConnected ? (
+        <SepoliaInstructions />
+      ) : (
+        <>
+          <div className="flex flex-col justify-center items-center gap-5">
+            <Tag type="info" content="Add Sepolia testnet" />
+            <p className="text-center mas-menu-default w-60">
+              {Intl.t('connect-wallet.ressource-sidepanel.add-sepolia')}
+            </p>
+            <a
+              className="mas-menu-underline"
+              href="www.mylink.com"
+              target="_blank"
+            >
+              {Intl.t('general.click-here')}
+            </a>
+          </div>
+          <div className="w-full border-t border-info/5" />
+          <div className="flex flex-col justify-center items-center gap-5">
+            <Tag type="success" content="Create a Massa Wallet" />
+            <p className="text-center mas-menu-default w-56">
+              {Intl.t('connect-wallet.ressource-sidepanel.add-massa')}
+            </p>
+            <a
+              className="mas-menu-underline"
+              href={linkToCreateWallet}
+              target="_blank"
+            >
+              {Intl.t('general.click-here')}
+            </a>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/ConnectWalletPopup/ConnectWalletPopup.tsx
+++ src/components/ConnectWalletPopup/ConnectWalletPopup.tsx
@@ -10,38 +10,24 @@ import Intl from '@/i18n/i18n';
 export function ConnectWalletPopup({ ...props }) {
   const { setOpen } = props;
 
-  const args = {
-    onOpen: () => {
-      return null;
-    },
-    onClose: () => {
-      setOpen(false);
-    },
-  };
-
   return (
-    <>
-      <PopupModal
-        customClass="w-[720px] min-w-fit"
-        customClassNested="border border-tertiary bg-secondary/50 backdrop-blur-lg"
-        fullMode={true}
-        onOpen={args.onOpen}
-        onClose={args.onClose}
-      >
-        <PopupModalHeader>
-          <div className="text-f-primary flex flex-col mb-4">
-            <label className="mas-title mb-4">
-              {Intl.t('connect-wallet.title')}
-            </label>
-            <div className="mas-body">
-              {Intl.t('connect-wallet.description')}
-            </div>
-          </div>
-        </PopupModalHeader>
-        <PopupModalContent>
-          <ConnectWalletCards />
-        </PopupModalContent>
-      </PopupModal>
-    </>
+    <PopupModal
+      customClass="w-fit max-w-4xl"
+      customClassNested="border border-tertiary bg-secondary/50 backdrop-blur-lg"
+      fullMode={true}
+      onClose={() => setOpen(false)}
+    >
+      <PopupModalHeader>
+        <div className="text-f-primary flex flex-col mb-4">
+          <label className="mas-title mb-4">
+            {Intl.t('connect-wallet.title')}
+          </label>
+          <div className="mas-body">{Intl.t('connect-wallet.description')}</div>
+        </div>
+      </PopupModalHeader>
+      <PopupModalContent>
+        <ConnectWalletCards />
+      </PopupModalContent>
+    </PopupModal>
   );
 }
