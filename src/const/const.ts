af56efefa46767afbbeb275c9b06cc3597092f21
Pierre Seznec <petterseznec@gmail.com>
2024-01-04 12:50:09 +0100

    update contracts

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,23 +1,23 @@
 export const MASSA_WALLET = 'Massa Wallet';
 export const MASSA_WALLET_PROVIDER = 'massaWalletProvider';
 export const MASSA_STATION = 'MASSASTATION';
-export const EVM_BRIDGE_ADDRESS = '0x86506DEb22c0285a0D9a5a770177176343a3E3bd';
+export const EVM_BRIDGE_ADDRESS = '0x141631E69E65c697c142f56c6fdCaF4ec648390e';
 
 // SMART CONTRACTS ADDRESSES
 export const CONTRACT_ADDRESS =
-  'AS1eY3uaFi19RZ6yzhPUQSWyJAJCGH2GNzZKruSTotRzXV6ZKJQg';
+  'AS12UwPZ3EdM2sLX37zMdVH46McZohZRDbS4AhrfbfxtajRawD4jr';
 
 export const TDAI_CONTRACT_ADDRESS =
   '0x53844F9577C2334e541Aec7Df7174ECe5dF1fCf0';
 
 export const TDAI_MASSA_ADDRESS =
-  'AS187bzqFt9iscTvHZi34uU1u162DnNaxqffWq3HYGQ9jiHz4ukv';
+  'AS1xzTQiRSpq7hNzgPRJcaSLEU4qjaRZPx5JfFVywsJxpWprndtL';
 
 export const WETH_CONTRACT_ADDRESS =
   '0xf6E9FBff1CF908f6ebC1a274f15F5c0985291424';
 
 export const WETH_MASSA_ADDRESS =
-  'AS1koirPmNwhAhJmfMb91Lb7xhB5sv5jLb7YKtsuRnWpVhzg5Kts';
+  'AS12VrdTkW9EAt5ySsnhxKv33gr38hiRm22b63qR7r4h2JQQpzgad';
 
 export const U256_MAX = BigInt(
   '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
