7e8bf11f96f23e7c42a2d1607cc254deff9bde60
Pierre Seznec <34547263+peterjah@users.noreply.github.com>
2024-01-12 14:41:17 +0100

    handle insufficient MAS balance toast (#358)

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -7,22 +7,23 @@ export interface CustomError extends Error {
   };
 }
 
-export const ERRORS_MESSAGES = [
+const ERRORS_MESSAGES = [
   'unable to unprotect wallet',
   'TransactionExecutionError: User rejected the request',
 ];
 
-export const WARNING_MESSAGES = [
+const WARNING_MESSAGES = [
   'signing operation: calling executeHTTPRequest for call: aborting during HTTP request',
 ];
 
-export const PARAMETER_ERROR = [
+const PARAMETER_ERROR = [
   'ContractFunctionExecutionError: Execution reverted for an unknown reason',
 ];
 
 export const ERROR_API = '400';
 
 export const EMPTY_API_RESPONSE = '401';
+const BALANCE_ERROR = 'does not have enough balance to pay';
 
 export const regexErr = new RegExp(ERRORS_MESSAGES.join('|'), 'i');
 export const regexWarn = new RegExp(WARNING_MESSAGES.join('|'), 'i');
@@ -42,4 +43,8 @@ export function isApiUrlError(error: CustomError): boolean {
 
 export function isEmptyApiResponse(error: CustomError): boolean {
   return error.message === EMPTY_API_RESPONSE;
+}
+
+export function isInsufficientBalanceError(error: Error): boolean {
+  return !!error?.toString().includes(BALANCE_ERROR);
 }
