e0b80a08dd8d84d10404e6046bf6bc375f6693fa
Pierre Seznec <petterseznec@gmail.com>
2024-01-18 15:31:12 +0100

    listen to network changes

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,15 +1,33 @@
+import {
+  MAINNET as web3Mainnet,
+  BUILDNET as web3Buildnet,
+} from '@massalabs/massa-web3';
 import { ETH_MAINNET_CHAIN_ID, SEPOLIA_CHAIN_ID } from './const';
 
+export const MassaNetworks = {
+  mainnet: web3Mainnet.toLowerCase(),
+  buildnet: web3Buildnet.toLowerCase(),
+};
+
 // function that validate the connected network is the right one
-export function validateNetwork(
+export function validateEvmNetwork(
   isMainnet: boolean,
-  evmChainId?: number,
+  evmChainId: number,
 ): boolean {
-  if (
-    (isMainnet && evmChainId !== ETH_MAINNET_CHAIN_ID) ||
-    (!isMainnet && evmChainId !== SEPOLIA_CHAIN_ID)
-  ) {
-    return false;
+  if (isMainnet) {
+    return evmChainId === ETH_MAINNET_CHAIN_ID;
+  } else {
+    return evmChainId === SEPOLIA_CHAIN_ID;
   }
-  return true;
+}
+
+export function validateMassaNetwork(
+  isMainnet: boolean,
+  massaNetwork: string,
+): boolean {
+  if (isMainnet) {
+    return massaNetwork === MassaNetworks.mainnet;
+  } else {
+    return massaNetwork === MassaNetworks.buildnet;
+  }
 }
