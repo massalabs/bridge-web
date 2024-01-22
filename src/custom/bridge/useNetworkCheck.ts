050dc1964b17622b1e2defe2ddfae19beb8f04eb
Nathan Seva <thykof@protonmail.ch>
2024-01-22 13:37:13 +0100

    fix network toasters (#403)

---
+++ src/custom/bridge/useNetworkCheck.ts
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import { toast } from '@massalabs/react-ui-kit';
+import { useNetwork } from 'wagmi';
+
+import { validateEvmNetwork, validateMassaNetwork } from '../../utils/network';
+import Intl from '@/i18n/i18n';
+import { useAccountStore, useBridgeModeStore } from '@/store/store';
+
+export function useNetworkCheck(
+  setWrongNetwork: (value: boolean) => void,
+): void {
+  const { connectedNetwork } = useAccountStore();
+  const { chain: evmConnectedChain } = useNetwork();
+  const [isMainnet, currentMode] = useBridgeModeStore((state) => [
+    state.isMainnet,
+    state.currentMode,
+  ]);
+
+  const [toastIdEvm, setToastIdEvm] = useState<string>('');
+  const [toastIdMassa, setToastIdMassa] = useState<string>('');
+
+  useEffect(() => {
+    // Check and toast EVM
+    let evmOk = false;
+    if (
+      evmConnectedChain &&
+      !validateEvmNetwork(isMainnet, evmConnectedChain.id)
+    ) {
+      console.log('toast evm');
+      setToastIdEvm(
+        toast.error(Intl.t('connect-wallet.wrong-evm-chain'), { id: 'evm' }),
+      );
+      setWrongNetwork(true);
+    } else {
+      console.log('dismiss evm');
+      toast.dismiss(toastIdEvm);
+      evmOk = true;
+    }
+
+    // Check and toast Massa
+    let massaOk = false;
+    if (
+      connectedNetwork &&
+      !validateMassaNetwork(isMainnet, connectedNetwork)
+    ) {
+      console.log('toast massa');
+      setToastIdMassa(
+        toast.error(
+          Intl.t('connect-wallet.wrong-massa-chain', { id: 'massa' }),
+        ),
+      );
+      setWrongNetwork(true);
+    } else {
+      console.log('dismiss massa');
+      toast.dismiss(toastIdMassa);
+      massaOk = true;
+    }
+
+    setWrongNetwork(!massaOk || !evmOk);
+  }, [evmConnectedChain, currentMode, connectedNetwork]);
+}
