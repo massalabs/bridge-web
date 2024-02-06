f32345bbb475b5185565151001616ea5685a1749
Pierre Seznec <petterseznec@gmail.com>
2024-02-06 16:00:20 +0100

    add alchemy provider

--- src/const/wagmi.ts
+++ src/const/wagmi.ts
@@ -2,6 +2,7 @@ import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { metaMaskWallet } from '@rainbow-me/rainbowkit/wallets';
 import { publicProvider } from '@wagmi/core/providers/public';
 import { configureChains, createConfig, sepolia, mainnet } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { infuraProvider } from 'wagmi/providers/infura';
 
 export const { chains, publicClient } = configureChains(
@@ -9,6 +10,7 @@ export const { chains, publicClient } = configureChains(
   [
     publicProvider(),
     infuraProvider({ apiKey: import.meta.env['VITE_INFURA_API_KEY'] }),
+    alchemyProvider({ apiKey: import.meta.env['VITE_ALCHEMY_API_KEY'] }),
   ],
   { batch: { multicall: true }, retryCount: 50 },
 );
