fa767b3a03967bb775b9e50b5a86ce5567bc585a
Nathan Seva <thykof@protonmail.ch>
2024-02-12 14:15:08 +0100

    Refresh estimation (#504)
    * refetch estimation gas price
    
    * add missing env var in types

--- src/custom/api/useFeeEstimation.ts
+++ src/custom/api/useFeeEstimation.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useEstimateFeesPerGas } from 'wagmi';
 
 const VITE_CLAIM_GAS_COST = import.meta.env.VITE_CLAIM_GAS_COST || '92261';
@@ -5,9 +6,19 @@ const VITE_LOCK_GAS_COST = import.meta.env.VITE_LOCK_GAS_COST || '73185';
 const VITE_APPROVE_GAS_COST = import.meta.env.VITE_APPROVE_GAS_COST || '29823';
 
 export function useFeeEstimation() {
-  const { data } = useEstimateFeesPerGas();
+  const { data, refetch } = useEstimateFeesPerGas();
+  const [maxFeePerGas, setMaxFeePerGas] = useState(0n);
 
-  const maxFeePerGas = data?.maxFeePerGas || 0n;
+  useEffect(() => {
+    setMaxFeePerGas(data?.maxFeePerGas || 0n);
+    const intervalId = setInterval(() => {
+      refetch().then((newData) => {
+        setMaxFeePerGas(newData.data?.maxFeePerGas || 0n);
+      });
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, [data, refetch]);
 
   const estimateClaimFees = () => BigInt(VITE_CLAIM_GAS_COST) * maxFeePerGas;
 
