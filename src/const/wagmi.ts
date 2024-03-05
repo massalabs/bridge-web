ab3811776c097a5779dab15de447491ec30726c7
Pivi <pivi@Pivis-MacBook-Air.local>
2024-03-05 09:26:47 +0100

    Add BSC/BSCTESTNET support on wagmi

--- src/const/wagmi.ts
+++ src/const/wagmi.ts
@@ -1,11 +1,13 @@
-import { mainnet, sepolia } from 'viem/chains';
+import { bsc, bscTestnet, mainnet, sepolia } from 'viem/chains';
 import { createConfig, createStorage, http } from 'wagmi';
 
 export const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [mainnet, sepolia, bsc, bscTestnet],
   storage: createStorage({ storage: window.localStorage }),
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
+    [bsc.id]: http(),
+    [bscTestnet.id]: http(),
   },
 });
