65e27e4c11022318332f24f8719fc4cde9bf2659
Pivi <pivi@Pivis-MacBook-Air.local>
2024-03-07 10:34:59 +0100

    refacto WrongChain.tsx

--- src/components/Status/WrongChain/WrongChain.tsx
+++ src/components/Status/WrongChain/WrongChain.tsx
@@ -3,7 +3,7 @@ import { Tag, Tooltip } from '@massalabs/react-ui-kit';
 import { FiHelpCircle } from 'react-icons/fi';
 import { Blockchain, SUPPORTED_MASSA_WALLETS } from '@/const';
 import Intl from '@/i18n/i18n';
-import { useAccountStore, useBridgeModeStore } from '@/store/store';
+import { useAccountStore } from '@/store/store';
 import { tagTypes } from '@/utils/const';
 
 interface WrongChainProps {
@@ -12,22 +12,13 @@ interface WrongChainProps {
 
 export function WrongChain(props: WrongChainProps) {
   const { blockchain } = props;
-  const { isMainnet: getIsMainnet } = useBridgeModeStore();
-  const isMainnet = getIsMainnet();
   const { currentProvider } = useAccountStore();
 
-  // TODO: use Blockchain enum for networks
-
-  let network = '';
   let transKey = '';
   if (blockchain === Blockchain.MASSA && currentProvider) {
     // currentProvider is always defined when blockchain is MASSA
     const providerName = currentProvider.name();
-    if (isMainnet) {
-      network = 'Mainnet';
-    } else {
-      network = 'Buildnet';
-    }
+
     if (providerName === SUPPORTED_MASSA_WALLETS.MASSASTATION) {
       transKey = 'index.tag.wrong-chain-massa-station-tooltip';
     } else if (
@@ -38,18 +29,8 @@ export function WrongChain(props: WrongChainProps) {
     }
   } else if (blockchain === Blockchain.ETHEREUM) {
     transKey = 'connect-wallet.connect-metamask.invalid-network';
-    if (isMainnet) {
-      network = 'Mainnet';
-    } else {
-      network = 'Sepolia';
-    }
   } else if (blockchain === Blockchain.BSC) {
     transKey = 'connect-wallet.connect-metamask.invalid-network';
-    if (isMainnet) {
-      network = 'Mainnet';
-    } else {
-      network = 'Testnet';
-    }
   }
 
   return (
@@ -58,7 +39,7 @@ export function WrongChain(props: WrongChainProps) {
         <Tooltip
           className="w-fit p-0 hover:cursor-pointer"
           customClass="p-0 mas-caption w-fit whitespace-nowrap"
-          body={Intl.t(transKey, { network })}
+          body={Intl.t(transKey)}
         >
           <div className="flex items-center">
             <span className="mr-1">{Intl.t('index.tag.wrong-chain')}</span>
