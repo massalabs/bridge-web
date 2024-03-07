f672ca51ab8fa0c5ffe94f80178a196329822c25
Pivi <pivi@Pivis-MacBook-Air.local>
2024-03-07 16:07:12 +0100

    Add Massa Explo Buildnet Link

--- src/pages/HistoryPage/TxLinkToExplorers.tsx
+++ src/pages/HistoryPage/TxLinkToExplorers.tsx
@@ -1,5 +1,11 @@
 import { useBridgeModeStore } from '@/store/store';
-import { EVM_EXPLORER, MASSA_EXPLORER_URL, SIDE } from '@/utils/const';
+import {
+  EVM_EXPLORER,
+  MASSA_EXPLORER_URL,
+  MASSA_EXPLO_EXTENSION,
+  MASSA_EXPLO_URL,
+  SIDE,
+} from '@/utils/const';
 import { maskAddress } from '@/utils/massaFormat';
 
 interface TxLinkToExplorersProps {
@@ -9,17 +15,25 @@ interface TxLinkToExplorersProps {
 
 export function TxLinkToExplorers(props: TxLinkToExplorersProps) {
   const { outputId, side } = props;
-  const { currentMode } = useBridgeModeStore();
+  const { currentMode, isMainnet: getIsMainnet } = useBridgeModeStore();
   const isMassaToEvm = side === SIDE.MASSA_TO_EVM;
 
   if (outputId === null || outputId === undefined) return;
 
-  // We have no way of correctly displaying a buildnet tx
-  if (!isMassaToEvm && currentMode !== 'mainnet') return;
+  const isMainnet = getIsMainnet();
 
-  const explorerUrl = isMassaToEvm
-    ? `${EVM_EXPLORER[currentMode]}tx/${outputId}`
-    : `${MASSA_EXPLORER_URL}${outputId}`;
+  const evmExplorer = `${EVM_EXPLORER[currentMode]}tx/${outputId}`;
+
+  function getExplorerUrl() {
+    if (isMainnet) {
+      return isMassaToEvm ? evmExplorer : `${MASSA_EXPLORER_URL}${outputId}`;
+    }
+    return isMassaToEvm
+      ? evmExplorer
+      : `${MASSA_EXPLO_URL}${outputId}${MASSA_EXPLO_EXTENSION}`;
+  }
+
+  const explorerUrl = getExplorerUrl();
 
   return (
     <a className="flex gap-2 items-center" href={explorerUrl} target="_blank">
