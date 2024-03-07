4710407a856d74a4e8a820abc3379c6c829c323c
Pivi <pivi@Pivis-MacBook-Air.local>
2024-03-07 10:11:06 +0100

    fix sepolia dai and weth sizing

--- src/assets/SepoliaWethSvg.tsx
+++ src/assets/SepoliaWethSvg.tsx
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import sepoliaWethSvg from './SVG/sepolia_weth.svg';
 import { SVGProps } from './svgInterface';
+=======
+import { SVGProps } from './BNBSvg';
+import sepoliaWethSvg from './SVG/sepolia_weth.svg';
+>>>>>>> afed3f7 (fix sepolia dai and weth sizing)
 
 export function SepoliaWethSvg(props: SVGProps) {
   const { size } = props;
