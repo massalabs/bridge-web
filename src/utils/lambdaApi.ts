cc23a90041294fd34d6da84c7432d265bbb6d34d
Nathan Seva <thykof@protonmail.ch>
2024-03-08 17:47:38 +0100

    refactor, remove filter state in getBurnById

--- src/utils/lambdaApi.ts
+++ src/utils/lambdaApi.ts
@@ -86,7 +86,6 @@ export async function getBurnById(
             evmAddress,
             // entities: [Entities.Burn],
             inputOpId: burnOpId,
-            state: apiOperationStates.processing,
           },
         },
       );
