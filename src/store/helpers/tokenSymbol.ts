695858a3d4e5228ef1452095c49a94c75204e206
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2024-03-12 13:37:56 +0100

    fix logo rendering (#585)
    * fix logo rendering
    
    * review
    
    ---------
    
    Co-authored-by: Pivi <pivi@Pivis-MacBook-Air.local>

--- src/store/helpers/tokenSymbol.ts
+++ src/store/helpers/tokenSymbol.ts
@@ -1,12 +1,11 @@
-// TODO: when the symbol in the MASSA blockchain will be suffixed with '.e'
+// when the symbol in the MASSA blockchain will be suffixed with '.e'
 // we will remove the suffix for the EVM symbol
 // and keep the symbol as is for the MASSA blockchain
 
 export function getMASSASymbol(symbol: string) {
   return symbol;
 }
-
 export function getEVMSymbol(symbol: string) {
-  // remove the suffix '.e' from the symbol
-  return symbol.replace('.e', '');
+  // removes any ". + alphanumeric " from string
+  return symbol.replace(/\.[^.]+$/, '');
 }
