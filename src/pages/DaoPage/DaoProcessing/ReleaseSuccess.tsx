dbaae2a3a8c09a27ce6b843d3b0071dd3a4f6060
Nathan Seva <thykof@protonmail.ch>
2024-03-29 13:45:59 +0100

    wmas success

---
+++ src/pages/DaoPage/DaoProcessing/ReleaseSuccess.tsx
@@ -0,0 +1,44 @@
+import { useAccount } from 'wagmi';
+import { wmasSymbol } from '../DaoPage';
+import { Blockchain } from '@/const';
+import Intl from '@/i18n/i18n';
+import { useBridgeModeStore } from '@/store/store';
+
+interface ReleaseSuccessProps {
+  amount: string;
+}
+
+export function ReleaseSuccess(props: ReleaseSuccessProps) {
+  const { amount } = props;
+  const { chain } = useAccount();
+  const { evmNetwork: getEvmNetwork, massaNetwork: getMassaNetwork } =
+    useBridgeModeStore();
+  const massaNetwork = getMassaNetwork();
+  const evmNetwork = getEvmNetwork();
+
+  if (!chain) return null;
+
+  const massaChainAndNetwork = `${Blockchain.MASSA} ${Intl.t(
+    `general.${massaNetwork}`,
+  )}`;
+  const evmChainAndNetwork = `${chain.name} ${evmNetwork}`;
+
+  const emitter = evmChainAndNetwork;
+  const recipient = massaChainAndNetwork;
+
+  return (
+    <div className="flex flex-col gap-2 text-center">
+      <div>{Intl.t('dao-maker.success')}</div>
+      <div className="mas-subtitle p-2">
+        {amount} {wmasSymbol}
+      </div>
+      <div>
+        {Intl.t('index.loading-box.from-to', {
+          from: emitter,
+          to: recipient,
+        })}
+      </div>
+      <div>{Intl.t('dao-maker.success-received', { amount })}</div>
+    </div>
+  );
+}
