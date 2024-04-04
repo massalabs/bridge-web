b1fd67da2aa4d44f7129a0cd3f9b2514af5a2636
Nathan Seva <thykof@protonmail.ch>
2024-04-04 15:58:13 +0200

    add config for wmas bridge (#673)

--- src/const/const.ts
+++ src/const/const.ts
@@ -36,8 +36,9 @@ export const config = {
   [BridgeMode.mainnet]: {
     evmBridgeContract: '' as `0x${string}`,
     massaBridgeContract: '',
-    lambdaUrl: '',
-    wmas_address: '' as `0x${string}`,
+    lambdaUrl:
+      'https://gfltuhv4oouk5qs6jf7lv7tvp40vfwsj.lambda-url.eu-west-3.on.aws/',
+    wmas_address: '0xDc074966De429c92614769Dc6546A8E72E83175D' as `0x${string}`,
   },
   [BridgeMode.testnet]: {
     evmBridgeContract:
