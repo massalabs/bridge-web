28b98161616144363c686c2afff6a6d3b211900e
Nathan Seva <thykof@protonmail.ch>
2024-06-03 15:57:59 -0500

    fixup no const or enum in trans key

--- src/custom/bridge/useConnectorName.ts
+++ src/custom/bridge/useConnectorName.ts
@@ -1,9 +1,8 @@
 import { useAccount } from 'wagmi';
-import { Blockchain } from '@/const';
 import Intl from '@/i18n/i18n';
 
 export function useConnectorName() {
   const { connector } = useAccount();
 
-  return connector ? connector.name : Intl.t(`general.${Blockchain.UNKNOWN}`);
+  return connector ? connector.name : Intl.t('general.Unknown');
 }
