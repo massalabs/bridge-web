0889c6827c1591d95846af8dfb5cab5e28dd8876
Phoebe Lartisant <phoebelartisant@proton.me>
2024-06-06 16:42:51 +0200

    improve UX when chaning networks

--- src/pages/IndexPage/Layouts/BridgeRedeemLayout/BoxLayoutComponents/EvmBoxComponents.tsx
+++ src/pages/IndexPage/Layouts/BridgeRedeemLayout/BoxLayoutComponents/EvmBoxComponents.tsx
@@ -16,14 +16,22 @@ import {
 export function EVMHeader() {
   const { isConnected } = useAccount();
   const { currentMode, isMainnet: getIsMainnet } = useBridgeModeStore();
-  const { selectedEvm, setSelectedEvm, availableEvmNetworks } =
-    useOperationStore();
+  const {
+    selectedEvm,
+    setSelectedEvm,
+    availableEvmNetworks,
+    setInputAmount,
+    setOutputAmount,
+  } = useOperationStore();
   const { resetSelectedToken } = useTokenStore();
   const isMainnet = getIsMainnet();
 
   const walletName = useConnectorName();
 
   function handleChangeEvmNetwork(selectedEvm: SupportedEvmBlockchain) {
+    setInputAmount('');
+    setOutputAmount('');
+
     setSelectedEvm(selectedEvm);
     resetSelectedToken();
   }
