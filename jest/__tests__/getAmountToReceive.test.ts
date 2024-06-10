45d8792414ebb510a1892d84be4408ce6d7432bb
Nathan Seva <thykof@protonmail.ch>
2024-06-10 19:41:46 -0300

    fixup rename to getAmountToReceive

--- jest/__tests__/getAmountToReceive.test.ts
+++ jest/__tests__/getAmountToReceive.test.ts
@@ -1,6 +1,6 @@
 import { formatFTAmount } from '@massalabs/react-ui-kit';
 import { parseUnits } from 'viem';
-import { getAmountReceived, serviceFeeToPercent } from '../../src/utils/utils';
+import { getAmountToReceive, serviceFeeToPercent } from '../../src/utils/utils';
 
 describe('should calculate service fees', () => {
   afterEach(() => {
@@ -11,7 +11,7 @@ describe('should calculate service fees', () => {
     const amount = '0.004';
     const serviceFee = 10n;
     const decimals = 6;
-    const result = getAmountReceived(amount, serviceFee, decimals);
+    const result = getAmountToReceive(amount, serviceFee, decimals);
     expect(result).toBe('0.003996');
   });
 
@@ -19,7 +19,7 @@ describe('should calculate service fees', () => {
     const amount = '100';
     const serviceFee = 0n;
     const decimals = 6;
-    const result = getAmountReceived(amount, serviceFee, decimals);
+    const result = getAmountToReceive(amount, serviceFee, decimals);
     expect(result).toBe(amount);
   });
 
@@ -27,7 +27,7 @@ describe('should calculate service fees', () => {
     const amount = '100';
     const serviceFee = 10n;
     const decimals = 6;
-    const result = getAmountReceived(amount, serviceFee, decimals);
+    const result = getAmountToReceive(amount, serviceFee, decimals);
     expect(result).toBe('99.900000');
   });
 
@@ -35,7 +35,7 @@ describe('should calculate service fees', () => {
     const amount = '5618.897000';
     const serviceFee = 20n;
     const decimals = 6;
-    const result = getAmountReceived(amount, serviceFee, decimals);
+    const result = getAmountToReceive(amount, serviceFee, decimals);
     expect(result).toBe('5,607.659206');
   });
 
@@ -43,7 +43,7 @@ describe('should calculate service fees', () => {
     const amount = '101299120121.128893';
     const serviceFee = 20n;
     const decimals = 6;
-    const result = getAmountReceived(amount, serviceFee, decimals);
+    const result = getAmountToReceive(amount, serviceFee, decimals);
     expect(result).toBe('101,096,521,880.886640');
   });
 
@@ -51,7 +51,7 @@ describe('should calculate service fees', () => {
     const amount = Number.MAX_SAFE_INTEGER.toString();
     const serviceFee = 20n;
     const decimals = 18;
-    const result = getAmountReceived(amount, serviceFee, decimals);
+    const result = getAmountToReceive(amount, serviceFee, decimals);
 
     const _amount = parseUnits(amount, decimals);
     const redeemFee = (_amount * serviceFee) / 10000n;
@@ -64,34 +64,11 @@ describe('should calculate service fees', () => {
     expect(result).toBe(expected);
   });
 
-  describe('test service fee correspondence function', () => {
-    test('should return 0n as 0%', () => {
-      const serviceFee = 0n;
-      const result = serviceFeeToPercent(serviceFee);
-      expect(result).toBe('0%');
-    });
-
-    test('should return 10n as 0.01%', () => {
-      const serviceFee = 10n;
-      const result = serviceFeeToPercent(serviceFee);
-      expect(result).toBe('0.01%');
-    });
-
-    test('should return 44n as 0.044%', () => {
-      const serviceFee = 44n;
-      const result = serviceFeeToPercent(serviceFee);
-      expect(result).toBe('0.044%');
-    });
-    test('should return 500n as 0.5%', () => {
-      const serviceFee = 500n;
-      const result = serviceFeeToPercent(serviceFee);
-      expect(result).toBe('0.5%');
-    });
-
-    test('should return 100000n as 100%', () => {
-      const serviceFee = 100000n;
-      const result = serviceFeeToPercent(serviceFee);
-      expect(result).toBe('100%');
-    });
+  describe('serviceFeeToPercent', () => {
+    expect(serviceFeeToPercent(0n)).toBe('0%');
+    expect(serviceFeeToPercent(10n)).toBe('0.1%');
+    expect(serviceFeeToPercent(44n)).toBe('0.44%');
+    expect(serviceFeeToPercent(500n)).toBe('5%');
+    expect(serviceFeeToPercent(10000n)).toBe('100%');
   });
 });
