4876ebd1822c612158237a55d4196ca7fd7b7112
Nathan Seva <thykof@protonmail.ch>
2024-06-10 19:44:49 -0300

    refactor ServiceFeeTooltip

--- src/components/ServiceFeeTooltip/ServiceFeeTooltip.tsx
+++ src/components/ServiceFeeTooltip/ServiceFeeTooltip.tsx
@@ -1,36 +1,42 @@
 import { Tooltip } from '@massalabs/react-ui-kit';
+import { FiInfo } from 'react-icons/fi';
 import Intl from '@/i18n/i18n';
 
 export interface ServiceFeeTooltipProps {
-  input: string;
+  inputAmount: string;
+  outputAmount?: string;
   serviceFee: string;
-  output: string;
   symbol: string;
 }
 
 export function ServiceFeeTooltip(props: ServiceFeeTooltipProps) {
-  const { input, serviceFee, output, symbol } = props;
-  const serviceFeeTooltipBody = (
-    <div className="flex items-center gap-4">
-      <div className="flex flex-col gap-2">
-        <div>{Intl.t('service-fee.sending')}</div>
-        {input} {symbol}
-      </div>
-      <div>-</div>
-      <div className="flex flex-col gap-2">
-        <div>
-          {Intl.t('service-fee.fee', {
-            fee: serviceFee,
-          })}
+  const { inputAmount, outputAmount = '-', serviceFee, symbol } = props;
+
+  function ServiceFeeTooltipBody() {
+    return (
+      <div className="flex items-center gap-4">
+        <div className="flex flex-col gap-2">
+          <div>{Intl.t('service-fee.sending')}</div>
+          {inputAmount} {symbol}
         </div>
-        {output} {symbol}
+        <div>-</div>
+        <div className="flex flex-col gap-2">
+          <div>
+            {Intl.t('service-fee.fee', {
+              fee: serviceFee,
+            })}
+          </div>
+          {outputAmount} {symbol}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 
   return (
     <div className="flex items-center gap-2">
-      <Tooltip customClass="py-2" body={serviceFeeTooltipBody} />
+      <Tooltip customClass="py-2" body={<ServiceFeeTooltipBody />}>
+        <FiInfo className="mr-1" size={18} />
+      </Tooltip>
     </div>
   );
 }
