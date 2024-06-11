b8e9a13de2d37bba6051ce2fc060590e901818aa
Phoebe Lartisant <phoebelartisant@proton.me>
2024-06-11 15:59:28 +0200

    Revert "refacto getAmountToReceive and edit tests"
    This reverts commit 0257727ac37297afd61552068ed0e10e7875886e.

--- jest/__tests__/getAmountToReceive.test.ts
+++ jest/__tests__/getAmountToReceive.test.ts
@@ -7,12 +7,12 @@ describe('should calculate service fees', () => {
     jest.clearAllMocks();
   });
 
-  test('should return 0.004 - a 0.1% service fee', () => {
+  test('should return 0.01% of 0.004', () => {
     const amount = '0.004';
     const serviceFee = 10n;
     const decimals = 6;
     const result = getAmountToReceive(amount, serviceFee, decimals);
-    expect(result).toBe(3996n);
+    expect(result).toBe('0.003996');
   });
 
   test('should return input amount when service fee is 0n', () => {
@@ -23,28 +23,28 @@ describe('should calculate service fees', () => {
     expect(result).toBe(amount);
   });
 
-  test('should return 100 - 0.1% of service fees', () => {
+  test('should return 0.01% of 100', () => {
     const amount = '100';
     const serviceFee = 10n;
     const decimals = 6;
     const result = getAmountToReceive(amount, serviceFee, decimals);
-    expect(result).toBe(99900000n);
+    expect(result).toBe('99.900000');
   });
 
-  test('should return 5618.897000 - 0.02% of service fee', () => {
+  test('should return 0.02% of 5618.897000', () => {
     const amount = '5618.897000';
     const serviceFee = 20n;
     const decimals = 6;
     const result = getAmountToReceive(amount, serviceFee, decimals);
-    expect(result).toBe(5607659206n);
+    expect(result).toBe('5,607.659206');
   });
 
-  test('should return  101299120121.128893 - 0.02% of service fees', () => {
+  test('should return 0.02% of 101299120121.128893', () => {
     const amount = '101299120121.128893';
     const serviceFee = 20n;
     const decimals = 6;
     const result = getAmountToReceive(amount, serviceFee, decimals);
-    expect(result).toBe(101096521880886636n);
+    expect(result).toBe('101,096,521,880.886640');
   });
 
   test('should calculate 0.02% of MAX SAFE INT', () => {
@@ -56,8 +56,12 @@ describe('should calculate service fees', () => {
     const _amount = parseUnits(amount, decimals);
     const redeemFee = (_amount * serviceFee) / 10000n;
     const expectedReceivedAmount = _amount - redeemFee;
+    const expected = formatFTAmount(
+      expectedReceivedAmount,
+      decimals,
+    ).amountFormattedFull;
 
-    expect(result).toBe(expectedReceivedAmount);
+    expect(result).toBe(expected);
   });
 
   describe('serviceFeeToPercent', () => {
