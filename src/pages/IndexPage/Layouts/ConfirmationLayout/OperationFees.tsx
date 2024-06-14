4976608bf362bc69dbc672ee59820475c477a4b2
Phoebe Lartisant <phoebelartisant@proton.me>
2024-06-14 16:23:58 +0200

    add fees to confirmation page

--- src/pages/IndexPage/Layouts/ConfirmationLayout/OperationFees.tsx
+++ src/pages/IndexPage/Layouts/ConfirmationLayout/OperationFees.tsx
@@ -1,21 +1,31 @@
-import { useAccount } from 'wagmi';
+import { formatAmount } from '@massalabs/react-ui-kit';
+import { useAccount, useBalance } from 'wagmi';
+import { EstimatedAmount } from '@/components/EstimatedAmount';
+import { formatTotalMasFees } from '@/custom/api/useMassaFeeEstimation';
 import { useServiceFee } from '@/custom/bridge/useServiceFee';
 import Intl from '@/i18n/i18n';
+import { useOperationStore } from '@/store/operationStore';
 import { serviceFeeToPercent } from '@/utils/utils';
 export function OperationFees() {
   const { serviceFee } = useServiceFee();
-  const { chain } = useAccount();
+  const { chain, address } = useAccount();
+  const { feesMAS, feesETH, storageMAS } = useOperationStore();
+  const { data: balanceData } = useBalance({
+    address,
+  });
 
-  // waiting for FeeEstimation refactor
   return (
     <div className="mas-caption flex flex-col gap-2">
       <div className="flex items-center w-full justify-between">
         <div>{`${chain?.name} ${Intl.t('confirmation.fees')}`}</div>
-        <div>1000</div>
+        <EstimatedAmount
+          amount={formatAmount(feesETH || '', 18).full}
+          symbol={balanceData?.symbol}
+        />
       </div>
       <div className="flex items-center w-full  justify-between">
         <div>{`${Intl.t('general.Massa')} ${Intl.t('confirmation.fees')}`}</div>
-        <div>0</div>
+        <div>{formatTotalMasFees(feesMAS, storageMAS)}</div>
       </div>
       <div className="flex items-center w-full  justify-between">
         <div>{Intl.t('confirmation.service-fee')}</div>
