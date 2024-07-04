2e9188d3bb0ae50e004aa8b495c7a70eb2c2ebbd
Pivi Lartisant <pl@massa.net>
2024-07-04 15:40:27 +0200

    Fix service fee display (#754)
    * revamp servuce fees display
    
    * use MASSA TOKEN const
    
    * fix test

--- src/pages/HistoryPage/Received.tsx
+++ src/pages/HistoryPage/Received.tsx
@@ -1,6 +1,6 @@
 import { formatAmount } from '@massalabs/react-ui-kit';
 import { ServiceFeeTooltip } from '@/components/ServiceFeeTooltip/ServiceFeeTooltip';
-import { retrievePercent } from '@/utils/utils';
+import { getServiceFeeAmount, retrievePercent } from '@/utils/utils';
 
 interface AmountProps {
   inputAmount: string;
@@ -12,22 +12,25 @@ interface AmountProps {
 export function Received(props: AmountProps) {
   const { inputAmount, outputAmount, symbol = '', decimals = 9 } = props;
 
-  let outputPreview = '-';
   let outputFull = '-';
 
   if (outputAmount !== undefined && outputAmount !== null) {
     const formattedResult = formatAmount(outputAmount, decimals);
-    outputPreview = formattedResult.preview;
     outputFull = formattedResult.full;
   }
 
+  const serviceFeeAmount = getServiceFeeAmount(
+    BigInt(inputAmount),
+    retrievePercent(inputAmount, outputAmount).serviceFee,
+  );
+
   return (
     <div className="flex gap-2 items-center">
-      {outputPreview} {symbol}
+      {outputFull} {symbol}
       <ServiceFeeTooltip
         inputAmount={formatAmount(inputAmount, decimals).full}
-        outputAmount={outputFull}
-        serviceFee={retrievePercent(inputAmount, outputAmount)}
+        serviceFeeAmount={formatAmount(serviceFeeAmount, decimals).full}
+        serviceFeePercent={retrievePercent(inputAmount, outputAmount).percent}
         symbol={symbol || ''}
       />
     </div>
