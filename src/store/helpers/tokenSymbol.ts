c2db9fc123c045019ff75b3018b7ca770101701d
Nathan Seva <thykof@protonmail.ch>
2024-07-17 11:28:48 -0500

    ETH for bsc WETH

--- src/store/helpers/tokenSymbol.ts
+++ src/store/helpers/tokenSymbol.ts
@@ -3,5 +3,6 @@ export function getMASSASymbol(symbol: string) {
 }
 export function getEVMSymbol(symbol: string) {
   // removes any ". + alphanumeric " from string
+  if (symbol === 'WETH.b') return 'ETH';
   return symbol.replace(/\.[^.]+$/, '');
 }
