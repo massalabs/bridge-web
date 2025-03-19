515a2ff455431991c4785faf3d1110c91fcaf84b
Peterjah <ps@massa.net>
2025-03-19 15:55:41 +0100

    get BSC rpc from env

--- src/const/wagmi.ts
+++ src/const/wagmi.ts
@@ -1,6 +1,6 @@
 import { bsc, bscTestnet, mainnet, sepolia } from 'viem/chains';
 import { createConfig, createStorage, http } from 'wagmi';
-const { VITE_MAINNET_RPC } = import.meta.env;
+const { VITE_MAINNET_RPC, VITE_BSC_RPC } = import.meta.env;
 
 export const config = createConfig({
   chains: [mainnet, sepolia, bsc, bscTestnet],
@@ -8,7 +8,7 @@ export const config = createConfig({
   transports: {
     [mainnet.id]: http(VITE_MAINNET_RPC ?? mainnet.rpcUrls.default.http[0]),
     [sepolia.id]: http('https://sepolia.drpc.org'),
-    [bsc.id]: http(),
+    [bsc.id]: http(VITE_BSC_RPC ?? bsc.rpcUrls.default.http[0]),
     [bscTestnet.id]: http(),
   },
 });
