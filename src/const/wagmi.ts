1eae64c3f1ca0087e4408e1c47cb4d2cbe6cc348
Peterjah <ps@massa.net>
2025-09-10 09:41:30 +0200

    use custom default RPCs

--- src/const/wagmi.ts
+++ src/const/wagmi.ts
@@ -2,13 +2,16 @@ import { bsc, bscTestnet, mainnet, sepolia } from 'viem/chains';
 import { createConfig, createStorage, http } from 'wagmi';
 const { VITE_MAINNET_RPC, VITE_BSC_RPC } = import.meta.env;
 
+const DEFAULT_MAINNET_RPC = 'https://eth.llamarpc.com';
+const DEFAULT_BSC_RPC = 'https://bsc.drpc.org';
+
 export const config = createConfig({
   chains: [mainnet, sepolia, bsc, bscTestnet],
   storage: createStorage({ storage: window.localStorage }),
   transports: {
-    [mainnet.id]: http(VITE_MAINNET_RPC ?? mainnet.rpcUrls.default.http[0]),
+    [mainnet.id]: http(VITE_MAINNET_RPC ?? DEFAULT_MAINNET_RPC),
     [sepolia.id]: http('https://sepolia.drpc.org'),
-    [bsc.id]: http(VITE_BSC_RPC ?? bsc.rpcUrls.default.http[0]),
+    [bsc.id]: http(VITE_BSC_RPC ?? DEFAULT_BSC_RPC),
     [bscTestnet.id]: http(),
   },
 });
